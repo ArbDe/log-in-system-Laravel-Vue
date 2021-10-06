7ee29391e57ccb2eb6e01497d65d817c59420bac
Adnit Gashi <arben.dedaj1@student.uni-pr.edu>
2021-10-06 22:35:47 +0200

    Login

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,27 +1,26 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '@/js/views/Home'
-import Register from '@/js/views/Register'
-import Login from '@/js/views/Login'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/js/views/Home";
+import Register from "@/js/views/Register";
+import Login from "./views/Login";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
+    mode: "history",
+    routes: [{
+            path: "/",
+            name: "home",
             component: Home
         },
         {
-            path: '/login',
-            name: 'login',
+            path: "/login",
+            name: "login",
             component: Login
         },
         {
-            path: '/register',
-            name: 'register',
+            path: "/register",
+            name: "register",
             component: Register
         }
     ]
---
+++ resources/js/views/Register.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <b-container fluid class="px-0 mx-0 my-2">
+      <b-row>
+        <b-col cols="12" md="4"></b-col>
+        <b-col cols="12" md="4">
+          <b-form @submit="onSubmit" @reset="onReset">
+            <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+              <b-form-input
+                id="input-1"
+                v-model="form.name"
+                type="text"
+                placeholder="Enter name"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+          </b-form>
+        </b-col>
+        <b-col cols="12" md="4"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      this.form.email = "";
+      this.form.name = "";
+    },
+  },
+};
+</script>
