89ec920fb3f45472a81d881eec3f2e0ec8b51a32
Adnit Gashi <arben.dedaj1@student.uni-pr.edu>
2021-10-07 16:32:53 +0200

    Nav component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "./views/Home";
+import Home from "./views/Home.vue";
 import Register from "./views/Register";
 import Login from "./views/Login";
 import User from "./views/User";
--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,46 +1,18 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">
-        <router-link to="/"> Home </router-link>
-      </b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item>
-            <router-link to="/login"> LogIn </router-link>
-          </b-nav-item>
-          <b-nav-item>
-            <router-link to="/register"> Register </router-link>
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <router-view></router-view>
+    <Nav />
   </div>
 </template>
 
 <script>
-export default {};
+import Nav from "../components/Nav.vue";
+import LogIn from "./Login.vue";
+import Register from "./Register.vue";
+
+export default {
+  components: { Nav, LogIn, Register },
+};
 </script>
 
 <style>
-.navbar-brand {
-  font-size: 1.5rem;
-}
-
-a.nav-link {
-  text-decoration: none;
-}
-a {
-  font-weight: bold;
-  color: #fef9ef;
-}
-a:hover {
-  text-decoration: none;
-  transition: 0.2s;
-  color: #b6b1a8;
-}
 </style>
